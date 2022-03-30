e48424f41cb7dcd8ac75b2fb44f21a183e0888df
siimonnnn <102421416+siimonnnn@users.noreply.github.com>
2022-03-30 23:19:17 +0200

    Skripte
    prva skripta napisana u notesu i ona je izmenjena skripta stare verzije za pomeranje.
    Imamo onda skriptu za base podataka karata tu skalidstimo sve karte,
    Za opis karata imamo skriptu nazvana je Card.
    Imamo neke pokusaje skripti za oduzimanje(Radili smo oduzimanje preko damga) i sabiranje(Public game obkest je nayiv skripte).

---
+++ skripte proba/public gameObject[] cardsInBox;.cs
@@ -0,0 +1,39 @@
+{
+public gameObject[] cardsInBox;
+     
+     float boxTotal = 0;
+     for (int i = 0; i < cardsInBox[].Length; i++)
+     {
+             boxTotal += cardsInBox[i].cardNumber about;
+     }
+     return boxTotal;
+ }
+
+ public int cardNumber = 1;
+ 
+ public int suitTexture = 0; 
+
+ void Randomizer()
+ {
+     if (Input.GetKeyDown(KeyCode.Space))
+     {
+     cardNumber = (int)Random.Range (1,13);
+     suitNumber = (int)Random.Range (0,4);
+     }
+ }
+
+
+  for (int j = 0; j < cardCount, j++)
+         {
+             for (int i = 0; i < cardCount, i++)
+             {
+                        
+                        
+                        
+            }
+         }
+
+        if (Input.GetKeyDown(Keycode.H))
+         {
+             cardCount++;
+         }
