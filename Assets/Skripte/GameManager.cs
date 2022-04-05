af3124d4890222edd0db7f7e901ab96e4f99fa2c
siimonnnn <102421416+siimonnnn@users.noreply.github.com>
2022-04-06 00:55:42 +0200

    Ubacio sam skriptu poljekarte u game menagera

--- Assets/Skripte/GameManager.cs
+++ Assets/Skripte/GameManager.cs
@@ -5,21 +5,52 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject karta;
-    //public int brojTrenutni1;
-    //public int brojTrenutni2;
-    // Start is called before the first frame update
+    public List<Karta> deck = new List<Karta>();
+    public Transform[] cardSlots;
+    public bool[] avaibleCardSlots;
+    public Text deckSizeText;
 
-    void Start()
+
+    public void DrawCard()
     {
-        //brojTrenutni1=20;
-        //brojTrenutni1=20;
-        for(int i=0;i<5;i++)
-            Instantiate(karta,new Vector3(-4+i,-5,0),Quaternion.identity);
+
+        if (deck.Count >= 1)
+        {
+            Karta randCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < avaibleCardSlots.Length; i++)
+            {
+                if (avaibleCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    avaibleCardSlots[i] = false;
+                    deck.Remove(randCard);
+                    return;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        deckSizeText.text = deck.Count.ToString();
     }
+
 }
+
+
+
+
+// public GameObject karta;
+//public int brojTrenutni1;
+//public int brojTrenutni2;
+// Start is called before the first frame update
+
+
+
+
+//brojTrenutni1=20;
+//brojTrenutni1=20;
+// for(int i=0;i<5;i++)
+//Instantiate(karta,new Vector3(-4+i,-5,0),Quaternion.identity);
